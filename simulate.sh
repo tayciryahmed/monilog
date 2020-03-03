0db7d372c38b86143971dff79f3d1af750876f15
Taycir Yahmed <taycir.yahmed@gmail.com>
2020-03-03 17:43:08 +0100

    add more details to simulate

--- simulate.sh
+++ simulate.sh
@@ -1,3 +1,3 @@
-rm /tmp/access.log
-python src/monilog/log_generator.py &
-python src/monilog/pipeline.py
+rm /tmp/access.log simulation*log
+python src/monilog/log_generator.py --rates 9 11 8 --durations 150 150 150 &
+python src/monilog/pipeline.py --file /tmp/access.log --threshold 10
