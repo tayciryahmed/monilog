b19ba9d45c2bb6679fb0a7814134b87f8314d41e
Taycir Yahmed <taycir.yahmed@gmail.com>
2020-03-05 00:06:50 +0100

    Update simulate.sh

--- simulate.sh
+++ simulate.sh
@@ -1,6 +1,5 @@
-pip uninstall -y monilog
-rm /tmp/access.log simulation*log
-python setup.py install 
+pip install monilog 
+
 log_generator --rates 9 11 8 --durations 150 150 150 &
 monitoring --file /tmp/access.log --threshold 10
 
