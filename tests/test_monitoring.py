e5dbf36f9613c82848e8eeb7367616c72a711a40
Taycir Yahmed <taycir.yahmed@gmail.com>
2020-03-04 22:14:21 +0000

    tests

--- tests/test_monitoring.py
+++ tests/test_monitoring.py
@@ -8,8 +8,8 @@ tmp_file = '/tmp/test'
 
 
 def test_run():
-    if not os.path.exists(tmp_file):
-        os.mknod(tmp_file)
+    if not os.path.exists('/tmp/access.log'):
+        os.mknod('/tmp/access.log')
     monitoring_pipeline = MonilogPipeline()
     monitoring_pipeline.stop_monitoring()
     monitoring_pipeline.run()
